108eaf5a933ebd55c2073caffc71aa81878e104c
admin <admin@DESKTOP-6H62P03>
2022-08-29 14:45:49 +0530

    update

--- src/main/java/practice1/TyScriptTest.java
+++ src/main/java/practice1/TyScriptTest.java
@@ -1,7 +1,10 @@
 package practice1;
 
-public class TyScript {
-	public static void main(String[] args) {
+import org.testng.annotations.Test;
+
+public class TyScriptTest {
+	@Test
+	public void test1() {
 		System.out.println("hi i m swati");
 		System.out.println("hello users");
 	}
