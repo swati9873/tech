6a383ff05255a3b64e1ff99bba30ef8dc21b19d3
admin <admin@DESKTOP-6H62P03>
2022-08-29 14:51:41 +0530

    wertyu

--- src/main/java/practice1/TyScriptTest.java
+++ src/main/java/practice1/TyScriptTest.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class TyScriptTest {
 	@Test
-	public void test1() {
+	public void asdfg_test1() {
 		System.out.println("hi i m swati");
 		System.out.println("hello users");
 	}
--- src/main/java/practice2/Script2Test.java
+++ src/main/java/practice2/Script2Test.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class Script2Test{
 	@Test
-	public void test() {
+	public void asdfgh_test() {
 		System.out.println("gm everyone");
 		System.out.println("hello");
 	}
