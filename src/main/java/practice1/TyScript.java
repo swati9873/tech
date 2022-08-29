d0cd8c201a68085c31829c70b97b6099fdaaffd8
admin <admin@DESKTOP-6H62P03>
2022-08-29 12:31:11 +0530

    yes

--- src/main/java/practice1/TyScript.java
+++ src/main/java/practice1/TyScript.java
@@ -3,7 +3,7 @@ package practice1;
 public class TyScript {
 	public static void main(String[] args) {
 		System.out.println("hi i m swati");
-		System.out.println("hello");
+		System.out.println("hello users");
 	}
 
 }
