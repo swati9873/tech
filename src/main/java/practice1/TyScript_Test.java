1e05fe84d630cbd8656140952274dd38dfb4a467
admin <admin@DESKTOP-6H62P03>
2022-08-29 18:07:23 +0530

    wertyu

--- src/main/java/practice1/TyScript_Test.java
+++ src/main/java/practice1/TyScript_Test.java
@@ -2,7 +2,7 @@ package practice1;
 
 import org.testng.annotations.Test;
 
-public class TyScriptTest {
+public class TyScript_Test {
 	@Test
 	public void asdfg_test1() {
 		System.out.println("hi i m swati");
--- src/main/java/practice2/Script2_Test.java
+++ src/main/java/practice2/Script2_Test.java
@@ -2,7 +2,7 @@ package practice2;
 
 import org.testng.annotations.Test;
 
-public class Script2Test{
+public class Script2_Test{
 	@Test
 	public void asdfgh_test() {
 		System.out.println("gm everyone");
